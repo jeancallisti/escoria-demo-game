5404bcaf01c97bf03d155c420627b674606f674a
monsieur xx <jeancallisti@yahoo.fr>
2023-01-30 12:58:29 +0100

    added plugin mouse position

---
+++ addons/mouse-position/mouse-position.gd
@@ -0,0 +1,17 @@
+tool
+extends EditorPlugin
+
+var dock
+
+func _enter_tree():
+	dock = preload("res://addons/mouse-position/plugin.tscn").instance()
+	add_control_to_dock(DOCK_SLOT_RIGHT_UR, dock)
+
+func _input(event):
+	var scene_root = get_tree().get_edited_scene_root()
+	var mouse_coords = scene_root.get_global_mouse_position()
+	dock.set_text(str(mouse_coords))
+
+func _exit_tree():
+	remove_control_from_docks(dock)
+	dock.free()
